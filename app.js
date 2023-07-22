9f8f84f99193d8b7130f141633e9133829c11d89
Kartikey <69628973+Kartiky246@users.noreply.github.com>
2023-07-22 16:02:55 +0530

    Delete app.js
    delete

--- app.js
+++
@@ -1,7 +0,0 @@
-const e = React.createElement("p",{id:'head'},'HELLO!');
-const root = ReactDOM.createRoot(document.getElementById('id'));
-
-console.log(e) // will give object where props field have attribute and children
-
-const div = React.createElement('div',{id:'parent'},React.createElement('div',{id:'child'},[React.createElement('h1',{id:'heading'},'Hello'),React.createElement('h1',{id:'heading'},'Hello')]));
-root.render(div); // convert react object into html
